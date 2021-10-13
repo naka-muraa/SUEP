d243c647e7c771014cbd2a1847d30b6a1ee62557
htnabe <n21m312@matsu.shimane-u.ac.jp>
2021-10-14 00:16:52 +0900

    remove codes for displaying log

--- appFunction/saveData.js
+++ appFunction/saveData.js
@@ -6,8 +6,6 @@ export async function saveData(dp) {
     const dp_Key = dp[0];
     const dp_Value = dp[1];
     await AsyncStorage.setItem(dp_Key, dp_Value);
-    console.log('ファイル名：saveData.js\n' + "ログ：保存成功\n");
   } catch (error) {
-    console.log("ファイル名：saveData.js\n" + "エラー：" + error + '\n');
   }
 }
