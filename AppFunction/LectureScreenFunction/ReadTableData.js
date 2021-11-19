6e9c85f94ac9048089152eb55973e8ba9ac5dc7e
htnabe <n21m312@matsu.shimane-u.ac.jp>
2021-11-19 15:52:39 +0900

    rename files

--- AppFunction/LectureScreenFunction/ReadTableData.js
+++ AppFunction/LectureScreenFunction/ReadTableData.js
@@ -5,10 +5,8 @@ export async function readTableData(rd) {
   try {
     const gbi = await AsyncStorage.getItem(rd);
     if (gbi != null) {
-      console.log('ファイル名：ReadTableData.js\n読み出し成功\n');
       return gbi;
     } else {
-      console.log('ファイル名：ReadTableData.js\n値無し\n');
       return null;
     }
   } catch (error) {
