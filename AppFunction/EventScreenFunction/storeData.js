592658cbbe99d55507c2970b73f0beab4f399a3f
htnabe <n21m312@matsu.shimane-u.ac.jp>
2021-11-30 10:52:37 +0900

    install Sentry on this project

--- AppFunction/EventScreenFunction/storeData.js
+++ AppFunction/EventScreenFunction/storeData.js
@@ -4,9 +4,9 @@ const storeData = async (value) => {
   try {
     const jsonValue = JSON.stringify(value);
     await AsyncStorage.setItem("store_fetchedData", jsonValue)
-  } catch (e) {
-    console.log(e);
-    console.log("ストレージへデータを保存する際にエラーが起きました");
+  } catch (error) {
+    console.log(error);
+    Sentry.Native.captureException(error);
   }
 }
 
